7982492595f9a918715b4e5a9f8891a87f984a20
cp3xzayn <cp3xzayn@gmail.com>
2021-07-10 19:47:09 +0900

    Buttonからオブジェクトを生成し、オブジェクトを選択できるようにした。

---
+++ Assets/Scripts/FieldMaker.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldMaker : MonoBehaviour
+{
+    /// <summary> FieldMakerのインスタンス </summary>
+    private static FieldMaker m_instance;
+    /// <summary> FieldMakerのインスタンス </summary>
+    public static FieldMaker Instance => m_instance;
+
+    private FieldMaker() { }
+
+    private FieldMakeState m_state;
+
+    public FieldMakeState State => m_state;
+
+    void Awake()
+    {
+        m_instance = this;
+        m_state = FieldMakeState.SelectObject;
+    }
+
+    /// <summary>
+    /// GameObjectを生成するボタンが押された時の処理
+    /// </summary>
+    /// <param name="objName"></param>
+    public void OnClickInstantiate(string objName)
+    {
+        Instantiate(Resources.Load<GameObject>(objName));
+    }
+
+    /// <summary>
+    /// stateを変更する
+    /// </summary>
+    /// <param name="state"></param>
+    public void SetState(FieldMakeState state)
+    {
+        m_state = state;
+        OnChangeState(state);
+    }
+
+    /// <summary>
+    /// stateが変わったときに一度だけ呼ばれる処理
+    /// </summary>
+    /// <param name="state"></param>
+    public void OnChangeState(FieldMakeState state)
+    {
+        switch (state)
+        {
+            case FieldMakeState.SelectObject:
+                Debug.Log("FieldMakeState.SelectObject");
+                break;
+            case FieldMakeState.ChangeState:
+                Debug.Log("FieldMakeState.ChangeState");
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+/// <summary>
+/// 現在の状態
+/// </summary>
+public enum FieldMakeState
+{
+    /// <summary> Objectを選択するState </summary>
+    SelectObject,
+    /// <summary> Objectの大きさ、位置を変更する </summary>
+    ChangeState
+}
---
+++ Assets/Scripts/ObjMove.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjMove : MonoBehaviour
+{
+    /// <summary> 選択されたObject </summary>
+    GameObject m_selectObject;
+
+    void Update()
+    {
+        switch (FieldMaker.Instance.State)
+        {
+            case FieldMakeState.SelectObject:
+                ObjectSelect();
+                break;
+            case FieldMakeState.ChangeState:
+                ObjectSelect();
+                break;
+            default:
+                break;
+        }
+    }
+
+
+    /// <summary>
+    /// Objectを選択する処理
+    /// </summary>
+    void ObjectSelect()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 10.0f))
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                m_selectObject = hit.collider.gameObject;
+                m_selectObject.GetComponent<Renderer>().material.color = Color.red;
+                Debug.Log("選択" + m_selectObject.name);
+                FieldMaker.Instance.SetState(FieldMakeState.ChangeState);
+            }
+        }
+    }
+
+    /// <summary>
+    /// 選択したObjectを移動する処理
+    /// </summary>
+    void ObjectMove()
+    {
+
+    }
+}
+
